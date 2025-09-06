37fa53f35ad3b7f07163a8fa435f4bcd10363137
matteo2302 <matteo.pizzichemi01@gmail.com>
2025-09-06 08:55:27 +0200

    testing header box shadows

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import CrowLogo from "../assets/CrowLogo.png"
+import styles from "./Header.module.css"
 
 
 function Header() {
 
 
     return (
-        <div><img src={CrowLogo} alt="Logo" className="headerLogo" />
-            <p>che chifo che chifo :(((</p>
-        </div>
+        <header className={styles.header}>
+            <img src={CrowLogo} alt="Logo" className={styles.headerlogo} />
+            <div className={styles.aboutmebox}>
+                <h1>Matteo Pizzichemix</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius expedita officiis labore, voluptates similique autem praesentium, optio dicta doloremque provident libero dolore amet exercitationem ipsum impedit, facere laborum. Quam, necessitatibus?</p>
+            </div>
+
+            <p className={styles.wifina}>che chifo che chifo :(((</p>
+        </header>
     )
 }
 
